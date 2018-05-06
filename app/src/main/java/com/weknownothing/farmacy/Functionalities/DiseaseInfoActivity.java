e49eb180c4e94cc3fc12196377bc12c4c12faf2b
shuja1497 <shujareshi1497@gmail.com>
2018-05-06 22:54:10 +0530

    layout added for showing disease info.

---
+++ app/src/main/java/com/weknownothing/farmacy/Functionalities/DiseaseInfoActivity.java
@@ -0,0 +1,15 @@
+package com.weknownothing.farmacy.Functionalities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.weknownothing.farmacy.R;
+
+public class DiseaseInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disease_info);
+    }
+}
