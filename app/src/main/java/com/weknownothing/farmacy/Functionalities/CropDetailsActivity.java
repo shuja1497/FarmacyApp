eebadebcc209598290b2ef4f1d3d2ac46cdf1926
TAANYA <id.tanyagupta@gmail.com>
2018-05-07 20:04:07 +0530

    CropDetails Updated

--- app/src/main/java/com/weknownothing/farmacy/Functionalities/CropDetailsActivity.java
+++ app/src/main/java/com/weknownothing/farmacy/Functionalities/CropDetailsActivity.java
@@ -1,8 +1,11 @@
 package com.weknownothing.farmacy.Functionalities;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,13 +17,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class CropDetailsActivity extends AppCompatActivity {
+public class CropDetailsActivity extends Activity {
 
     String crop;
     String crop_name;
     String gen_info;
     String soil_info;
     TextView crop_name_text , crop_ginfo_text , crop_sinfo_text;
+
+    CardView crop_name_cv , crop_ginfo_cv, crop_sinfo_cv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -35,6 +40,11 @@ public class CropDetailsActivity extends AppCompatActivity {
         crop_ginfo_text = (TextView)findViewById(R.id.crop_ginfo_text);
         crop_sinfo_text = (TextView)findViewById(R.id.crop_sinfo_text);
 
+        crop_name_cv = (CardView)findViewById(R.id.crop_name_cv);
+        crop_ginfo_cv = (CardView)findViewById(R.id.crop_ginfo_cv);
+        crop_sinfo_cv = (CardView)findViewById(R.id.crop_sinfo_cv);
+
+
 
         onReadCSV();
     }
@@ -43,11 +53,11 @@ public class CropDetailsActivity extends AppCompatActivity {
 
         try {
 
-            String csvFile = this.getApplicationInfo().dataDir + File.separatorChar + "info.csv";
+            String csvFile = this.getApplicationInfo().dataDir + File.separatorChar + "cropinfo.csv";
 
             Toast.makeText(getApplicationContext(), csvFile, Toast.LENGTH_LONG).show();
 
-            InputStreamReader inputStreamReader = new InputStreamReader(getResources().openRawResource(R.raw.info));
+            InputStreamReader inputStreamReader = new InputStreamReader(getResources().openRawResource(R.raw.cropinfo));
 
             CSVReader csvReader = new CSVReader(inputStreamReader, '|');
             String[] nextline;
@@ -57,17 +67,42 @@ public class CropDetailsActivity extends AppCompatActivity {
                 String ss = nextline[0].replace(" ", "");
                 if(ss.equalsIgnoreCase(crop))
                 {
-                    crop_name = nextline[1];
-                    gen_info = nextline[2];
-                    soil_info = nextline[3];
+
+                    crop_name = nextline[0];
+                    gen_info = nextline[1];
+                    soil_info = nextline[2];
                     break;
                 }
 
             }
 
-            crop_name_text.setText(crop_name);
-            crop_name_text.setText(gen_info);
-            crop_name_text.setText(soil_info);
+            crop_name = crop_name.replaceAll("\\s+", "");
+            if(!crop_name.replace(" ","").equalsIgnoreCase(""))
+            {
+                crop_name_cv.setVisibility(View.VISIBLE);
+                crop_name_text.setText(crop_name);
+            }
+            else
+                crop_name_cv.setVisibility(View.GONE);
+
+            gen_info = gen_info.replaceAll("\\s+", "");
+            if(!gen_info.replace(" ","").equalsIgnoreCase(""))
+            {
+                crop_ginfo_cv.setVisibility(View.VISIBLE);
+                crop_ginfo_text.setText(gen_info);
+            }
+            else
+                crop_ginfo_cv.setVisibility(View.GONE);
+
+            soil_info = soil_info.replaceAll("\\s+", "");
+            if(!soil_info.equals(""))
+            {
+                crop_sinfo_cv.setVisibility(View.VISIBLE);
+                crop_sinfo_text.setText(soil_info);
+            }
+            else
+                crop_sinfo_cv.setVisibility(View.GONE);
+
 
 
 
